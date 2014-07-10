005490837098c466bbdeaf0435935d6830df14e4
Naoya Maruyama <nmaruyama@riken.jp>
2014-07-10 15:52:22 +0900

    initial commit

---
+++ taco.py
@@ -0,0 +1,92 @@
+import itertools
+import pdb
+
+def product(s1, s2):
+    # itertools.product(s1, s2) does not work since it can include
+    # duplicated pairs
+    prod  = []
+    # covered_region_pair is a set of region pairs that are already
+    # inserted to the product list
+    covered_region_pair = set()
+    for i in s1:
+        for j in s2:
+            if (not (i.region, j.region) in covered_region_pair) and \
+               (not (j.region, i.region) in covered_region_pair):
+                prod.append((i, j))
+                covered_region_pair.add((i.region, j.region))
+    return prod
+
+class Particle(object):
+    def __init__(self,  pos=[0.0,0.0,0.0]):
+        self.pos = pos[:]
+
+    def __str__(self):
+        return "particle: " + ", ".join(str(i) for i in self.pos)
+    pass
+
+def distance_r2(p1, p2):
+    return reduce(lambda x, y: x + y,
+                  ((x - y) ** 2 for x, y
+                   in itertools.izip(p1.pos, p2.pos)))
+
+class Region(object):
+    def __init__(self, min_max1,
+                 min_max2, min_max3):
+        self.r = (min_max1, min_max2, min_max3)
+
+    def partition(self):
+        x = itertools.product(
+            *[[(r[0], r[0]+(r[1]-r[0])/2.0),
+             (r[0]+(r[1]-r[0])/2.0, r[1])]
+                 for r in self.r])
+        children = [Region(i[0], i[1], i[2]) for i in x]
+        return children
+
+    def contains(self, particle):
+        return all(p >= self.r[i][0] and p < self.r[i][1]
+                   for i, p in enumerate(particle.pos))
+
+    def __str__(self):
+        return "region: " + str(self.r)
+
+    def __eq__(self, other):
+        return self.r == other.r
+
+    def __ne__(self, other):
+        return not(self == other)
+
+    def __hash__(self):
+        return hash(self.r)
+        
+class Cell(object):
+    def __init__(self, particles, region, indices=None):
+        self.particles = particles
+        self.region = region
+        self.indices = indices
+        if self.indices is None:
+            self.indices = set()
+        pass
+
+    def __str__(self):
+        return "cell {" + str(self.region) + ", indices: " + \
+            str(self.indices) + "}"
+
+    # partition a cell to equally divided 8 sub cells
+    def partition(self):
+        child_regions = self.region.partition()
+        child_cells = []        
+        for c in child_regions:
+            child_cells.append(Cell(self.particles, c))
+
+        for index in self.indices:
+            for c in child_cells:
+                if c.region.contains(self.particles[index]):
+                    c.indices.add(index)
+
+        for c in child_cells:
+            print c
+        return child_cells
+
+    def get_num_particles(self):
+        return len(self.indices)
+    
