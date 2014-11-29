663db4fd956cfa9dcf35004b97ec31303f3e6912
Keisuke Fukuda <fukuda@matsulab.is.titech.ac.jp>
2014-11-29 15:18:44 +0900

    Changed function pointer to generic functor in tapas::Map

--- cpp/include/tapas/map.h
+++ cpp/include/tapas/map.h
@@ -6,20 +6,21 @@
 
 namespace tapas {
 
-template <class T1, class T2, class T1_Iter, class T2_Iter, class... Args>
-void Map(void (*f)(T1 &, T2 &, Args...),
-         ProductIterator<T1_Iter, T2_Iter> prod, Args...args) {
+/**
+ * Map function f over product of two iterators
+ */
+template <class Funct, class T1_Iter, class T2_Iter, class... Args>
+void Map(Funct f, ProductIterator<T1_Iter, T2_Iter> prod, Args...args) {
   TAPAS_LOG_DEBUG() << "map product iterator size: "
-                    << prod.size() << std::endl;  
+                    << prod.size() << std::endl;
   for (index_t i = 0; i < prod.size(); ++i) {
     f(prod.first(), prod.second(), args...);
     prod++;
   }
 }
-
-template <class T1, class T1_Iter, class... Args>
-void Map(void (*f)(T1 &, T1 &, Args...),
-         ProductIterator<T1_Iter> prod, Args...args) {
+  
+template <class Funct, class T1_Iter, class... Args>
+void Map(Funct f, ProductIterator<T1_Iter> prod, Args...args) {
   TAPAS_LOG_DEBUG() << "map product iterator size: "
                     << prod.size() << std::endl;  
   for (int i = 0; i < prod.size(); ++i) {
@@ -28,17 +29,18 @@ void Map(void (*f)(T1 &, T1 &, Args...),
   }
 }
 
-template <class T, class... Args>
-void Map(void (*f)(T &, Args...), SubCellIterator<T> iter, Args...args) {
+template <class Funct, class T, class... Args>
+void Map(Funct f, SubCellIterator<T> iter, Args...args) {
   TAPAS_LOG_DEBUG() << "map non-product subcell iterator size: "
-                    << iter.size() << std::endl;  
+                    << iter.size() << std::endl;
   for (int i = 0; i < iter.size(); ++i) {
     f(*iter, args...);
     iter++;
   }
 }
-template <class T, class... Args>
-void Map(void (*f)(BodyIterator<T> &, Args...), BodyIterator<T> iter, Args...args) {
+  
+template <class Funct, class T, class... Args>
+void Map(Funct f, BodyIterator<T> iter, Args...args) {
   TAPAS_LOG_DEBUG() << "map non-product body iterator size: "
                     << iter.size() << std::endl;  
   for (int i = 0; i < iter.size(); ++i) {
@@ -47,8 +49,8 @@ void Map(void (*f)(BodyIterator<T> &, Args...), BodyIterator<T> iter, Args...arg
   }
 }
 
-template <class T, class... Args>
-void Map(void (*f)(T &, Args...), T &x, Args...args) {
+template <class Funct, class T, class... Args>
+void Map(Funct f, T &x, Args...args) {
   TAPAS_LOG_DEBUG() << "map non-iterator" << std::endl;
   f(x, args...);
 }
