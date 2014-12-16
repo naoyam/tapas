7e26106f5a885ac008d3e6aa9c56b6f053d228b8
Keisuke Fukuda <fukuda@matsulab.is.titech.ac.jp>
2014-12-16 16:56:17 +0900

    Parallelized tapas::Map operation using MassiveThreads

--- cpp/include/tapas/map.h
+++ cpp/include/tapas/map.h
@@ -4,6 +4,13 @@
 #include "tapas/cell.h"
 #include "tapas/iterator.h"
 
+#ifdef MTHREAD
+# ifndef TO_MTHREAD_NATIVE
+#  define TO_MTHREAD_NATIVE 1
+# endif
+# include <tpswitch/tpswitch.h>
+#endif
+
 namespace tapas {
 
 /**
@@ -13,30 +20,57 @@ template <class Funct, class T1_Iter, class T2_Iter, class... Args>
 void Map(Funct f, ProductIterator<T1_Iter, T2_Iter> prod, Args...args) {
   TAPAS_LOG_DEBUG() << "map product iterator size: "
                     << prod.size() << std::endl;
+#if MTHREAD
+  mk_task_group;
+  for (int i = 0; i < prod.size(); i++) {
+    create_taskA( f(prod.first(), prod.second(), args...) );
+    prod++;
+  }
+  wait_tasks;
+#else
   for (index_t i = 0; i < prod.size(); ++i) {
     f(prod.first(), prod.second(), args...);
     prod++;
   }
+#endif
 }
   
 template <class Funct, class T1_Iter, class... Args>
 void Map(Funct f, ProductIterator<T1_Iter> prod, Args...args) {
   TAPAS_LOG_DEBUG() << "map product iterator size: "
-                    << prod.size() << std::endl;  
+                    << prod.size() << std::endl;
+#ifdef MTHREAD
+  mk_task_group;
+  for (int i = 0; i < prod.size(); i++) {
+    create_taskA( f(prod.first(), prod.second(), args...) );
+    prod++;
+  }
+  wait_tasks;
+#else
   for (int i = 0; i < prod.size(); ++i) {
     f(prod.first(), prod.second(), args...);
     prod++;
   }
+#endif
 }
 
 template <class Funct, class T, class... Args>
 void Map(Funct f, SubCellIterator<T> iter, Args...args) {
   TAPAS_LOG_DEBUG() << "map non-product subcell iterator size: "
                     << iter.size() << std::endl;
+#ifdef MTHREAD
+  mk_task_group;
+  for (int i = 0; i < iter.size(); i++) {
+    create_task0(f(*iter, args...));
+    iter++;
+  }
+  wait_tasks;
+#else
   for (int i = 0; i < iter.size(); ++i) {
     f(*iter, args...);
     iter++;
   }
+#endif
 }
   
 template <class Funct, class T, class... Args>
